94ae8396f77eb45a48909dce9da7d2db8826b49f
sohila558 <sohailat.441@gmail.com>
2025-08-09 20:56:47 +0300

    Collections

--- DemoADV02/Program.cs
+++ DemoADV02/Program.cs
@@ -4,7 +4,27 @@
     {
         static void Main(string[] args)
         {
-            
+            #region What is Collections ?
+            // Collections : Built-in DS in C#
+            // Array : Containers
+
+            // Collections in C#
+            // Lists
+            // Hashtables
+
+            // Generic - Non-Generic
+            // Lists
+            // Non-Generic : Array List - Satck - Queue
+            // Generic : List - Linked List - Stack - Queue
+
+            // Hashtables :
+            // Non-Generic : Hashtable
+            // Generic : Dictionary - SortedDictionary 
+            #endregion
+
+
+
+
         }
     }
 }
